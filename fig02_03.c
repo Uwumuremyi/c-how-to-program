011847f439419225cd3d8e56368d9ba7c4b5048e
jb <jeanu28@hotmail.com>
2017-11-14 17:17:24 -0500

    Add existing file

---
+++ fig02_03.c
@@ -0,0 +1,17 @@
+/* printing on one line with two printf statements */
+
+#include <stdio.h>
+
+/* function main begins program execution */
+
+int main( void )
+
+{
+
+	printf("Welcome ");
+
+	printf("to C!\n");
+
+	return 0; /* indicate that program ended successfully */
+
+}
