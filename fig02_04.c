9cd2b140c5ddf766805d5ec415713ca3d26eff96
jb <jeanu28@hotmail.com>
2017-11-14 17:43:50 -0500

    Add existing file

---
+++ fig02_04.c
@@ -0,0 +1,15 @@
+/* printing multiple lines with a single printf */
+
+#include <stdio.h>
+
+/* function main begins program execution */
+
+int main( void )
+
+{
+
+	printf("Welcome\nto\nC!\n");
+
+	return 0; /* indicate that program ended successfully */
+
+}
