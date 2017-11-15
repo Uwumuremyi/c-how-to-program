946771b41276cf0adc7d347dcc4e1a55b2b9eca1
jb <jeanu28@hotmail.com>
2017-11-15 14:10:46 -0500

    Add existing file

---
+++ chowtoprogram_02_16.c
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  Write a program that asks the user to enter two numbers, obtains them from the user and prints their sum, product, difference, quoti////  ent and remainder                                                                                                                   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main( void )
+
+{
+
+	int number1;
+
+	int number2;
+
+	int sum;
+
+	int product;
+
+	int difference;
+
+	int quotient;
+
+	int remainder;
+
+	/* get the user to input 2 numbers */
+	printf("Enter first number: \n");
+
+	scanf("%d", &number1);
+
+	printf("Enter second number: \n");
+
+	scanf("%d", &number2);
+
+
+	/* perfom calculations */
+	sum = number1 + number2;
+
+	product = number1 * number2;
+
+	difference = number1 - number2;
+
+	quotient = number1 / number2;
+
+	remainder = number1 % number2;
+
+	
+	/* display the results */
+	printf("Sum is %d\n", sum );
+
+	printf("Product is %d\n", product );
+
+	printf("Difference is %d\n", difference );
+
+	printf("Quotient is %d\n", quotient );
+
+	printf("Remainder is %d\n", remainder );
+
+	return 0;
+
+}
