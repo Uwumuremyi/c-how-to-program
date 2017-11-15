62075d42bfa041910dbb9fa27bd7688cebd1222a
jb <jeanu28@hotmail.com>
2017-11-15 16:32:49 -0500

    Add existing file

---
+++ chowtoprogram_02_17.c
@@ -0,0 +1,27 @@
+/* Write a program that prints the numbers 1 to 4 on the same line. Write the program using the following methods:
+ * a) Using one printf statement with no conversin specifier
+ * b) Using one printf statement with four conversion specifiers
+ * c) Using four printf statements
+ */
+
+#include <stdio.h>
+
+int main( void )
+
+{
+
+	printf("1 2 3 4\n");
+
+	printf("%d %d %d %d\n", 1 , 2, 3, 4 );
+
+	printf("1 ");
+
+	printf("2 ");
+
+	printf("3 ");
+
+	printf("4\n");
+
+	return 0;
+
+}
